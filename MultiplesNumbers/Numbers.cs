5463b3a6d3572296ae4599ca10086a24d37fd555
студент2 <2@DESKTOP-QEFI0G7>
2022-12-05 11:47:56 +0300

    Добавьте файлы проекта.

---
+++ MultiplesNumbers/Numbers.cs
@@ -0,0 +1,26 @@
+﻿namespace MultiplesNumbers;
+
+public class Numbers
+{
+    public void Func(int number)
+    {
+        int summ = 0;
+        for (int i = 1; i < number; i++)
+        {
+            if (i % 3 == 0)
+            {
+                summ += i;
+                Console.WriteLine();
+                Console.WriteLine($"{i} кратно трём");
+            }
+            else if (i % 5 == 0)
+            {
+                summ += i;
+                Console.WriteLine();
+                Console.WriteLine($"{i} кратно пяти");
+            }
+        }
+        Console.WriteLine();
+        Console.WriteLine($"Сумма всех кратных чисел {summ}");
+    }
+}
